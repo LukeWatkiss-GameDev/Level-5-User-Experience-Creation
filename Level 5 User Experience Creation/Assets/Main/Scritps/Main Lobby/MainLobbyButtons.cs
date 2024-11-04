0916d92067586f5b62e06b732332ee1b2be7d3b5
Luke Watkiss <LWatkissGameDev@gmail.com>
2024-11-04 17:53:33 +0000

    continued with lobby screen
    bottom buttons working

--- Level 5 User Experience Creation/Assets/Main/Scritps/Main Lobby/MainLobbyButtons.cs
+++ Level 5 User Experience Creation/Assets/Main/Scritps/Main Lobby/MainLobbyButtons.cs
@@ -6,11 +6,63 @@ using UnityEngine.UI;
 public class MainLobbyButtons : MonoBehaviour
 {
 
+    public GameObject playScreen,questsScreen,rhupassScreen,lockerScreen,shopScreen, careerScreen;
 
+    public GameObject currentScreen;
     public Button currentUpButton;
 
+    void Start()
+    {
+        SetPanelsNonActive();
+    }
 
-    public void ButtonSelected()
+    #region SwitchToX 
+    
+    public void SwitchToPlay()
+    {
+        ButtonSelected(playScreen);
+        
+    }
+    public void SwitchToQuests()
+    {
+        ButtonSelected(questsScreen);
+        
+    }
+    public void SwitchToRhupass()
+    {
+        ButtonSelected(rhupassScreen);
+        
+    }
+    public void SwitchToLocker()
+    {
+        ButtonSelected(lockerScreen);
+        
+    }
+    public void SwitchToShop()
+    {
+        ButtonSelected(shopScreen);
+        
+    }
+    public void SwitchToCareer()
+    {
+        ButtonSelected(careerScreen);
+        
+    }
+
+    #endregion
+
+/*   public void OpenFriendsModal()
+    {
+        SlidePanelIn(value);
+    }
+
+    public void OpenMenuModal()
+    {
+        SlidePanelIn(value);
+    } */
+
+
+    public void ButtonSelected(GameObject panel)
     {
         if(currentUpButton != null)
         {
@@ -22,12 +74,36 @@ public class MainLobbyButtons : MonoBehaviour
         currentUpButton = curButton;
         currentUpButton.interactable = false;
         MoveButtonUp(currentUpButton);
+
+        // disable the current active screen and then enable the new screen
+        currentScreen.SetActive(false);
+        currentScreen = panel;
+        currentScreen.SetActive(true);
         
         // will have to add changing panels somewhere in here
         // also find out which panel 
         // good luck 
 
     }
+    public void CloseModal()
+    {
+        // parent.parent will probably need to change
+        GameObject curPanel = EventSystem.current.currentSelectedGameObject.transform.parent.parent.gameObject;
+        //Debug.Log(curPanel);
+        curPanel.GetComponentInChildren<GrowAndShrinkText>().panelShrink(); // this will need to change 
+
+    }
+
+    void SliderPanelIn(float value)
+    {
+        
+    }
+
+    void SliderPanelout(float value)
+    {
+
+    }
+
 
     void MoveButtonDown(Button OBJ)
     {
@@ -37,6 +113,14 @@ public class MainLobbyButtons : MonoBehaviour
     void MoveButtonUp(Button OBJ)
     {
         OBJ.transform.DOLocalMoveY(-503.5f,0.5f);
+    }
+    void SetPanelsNonActive() // set all panels ,exept the main menu, to false
+    {
+        questsScreen.SetActive(false);
+        rhupassScreen.SetActive(false);
+        lockerScreen.SetActive(false);
+        shopScreen.SetActive(false);
+        careerScreen.SetActive(false);
     }
 
 
